4700bcc25c5d279466900f59269cf006e611c17e
mariusz <mariuszer@gmail.com>
2017-11-05 16:46:43 +0100

    Added translation

--- src/main/groovy/com/fpoon/s3dcam/Camera.groovy
+++ src/main/groovy/com/fpoon/s3dcam/Camera.groovy
@@ -4,18 +4,29 @@ import javafx.geometry.Point2D
 import javafx.geometry.Point3D
 
 import javax.swing.JPanel
+import java.awt.Color
 
 class Camera {
+
+    static final double TRANSLATION = 10.0
+    static final Point3D TRANSLATION_UP = new Point3D(0, +TRANSLATION, 0);
+    static final Point3D TRANSLATION_DOWN = new Point3D(0, -TRANSLATION, 0);
+    static final Point3D TRANSLATION_LEFT = new Point3D(-TRANSLATION, 0, 0);
+    static final Point3D TRANSLATION_RIGHT = new Point3D(+TRANSLATION, 0, 0);
+    static final Point3D TRANSLATION_FORWARD = new Point3D(0, 0, +TRANSLATION);
+    static final Point3D TRANSLATION_BACKWARD = new Point3D(0, 0, -TRANSLATION);
 
     JPanel panel
     double focal
     Scene scene;
+    Point3D translation;
 
 
     Camera(JPanel panel, Scene scene) {
         this.focal = 300.0
         this.panel = panel
         this.scene = scene
+        this.translation = new Point3D(0,0,0)
     }
 
     def render() {
@@ -24,7 +35,11 @@ class Camera {
 
     def render(Edge[] edges) {
         def gfx = panel.graphics
-        edges.collect {translate(it)} .each {
+        gfx.setColor(Color.BLACK)
+        gfx.fillRect(0, 0, panel.width, panel.height)
+        edges.collect {translatePoint(it)} .each {
+            if (!(Double.isFinite(it.begin.z) && Double.isFinite(it.end.z)))
+                return
             println "${it.begin} ${it.end}"
             gfx.color = it.color
             gfx.drawLine((int)it.begin.x, (int)it.begin.y, (int)it.end.x, (int)it.end.y);
@@ -32,20 +47,24 @@ class Camera {
 
     }
 
-    Edge translate(Edge edge3d) {
+    void translate(Point3D vector) {
+        translation = translation.add(vector)
+    }
+
+    private Edge translatePoint(Edge edge3d) {
         return new Edge(
-                translate(edge3d.begin),
-                translate(edge3d.end),
+                translatePoint(edge3d.begin),
+                translatePoint(edge3d.end),
                 edge3d.color
         )
     }
 
-    Point3D translate(Point3D p) {
+    private Point3D translatePoint(Point3D p) {
         def rect = panel.getVisibleRect()
-        def translate = new Point2D(rect.width / 2.0, rect.height / 2.0)
-        def mltp = focal / p.y
-        def x = mltp * p.getX() + translate.x
-        def z = translate.y - mltp * p.getZ()
-        return new Point3D(x, z, 0);
+        def windowTranslation = new Point3D(rect.width / 2.0, rect.height / 2.0, 0)
+        def mltp = focal / (p.z - translation.z)
+        def x = mltp * (p.x + translation.x) + windowTranslation.x
+        def y = windowTranslation.y - mltp * (p.y + translation.y)
+        return new Point3D(x, y, (p.z - translation.z) > 0 ? 0 : Double.NaN);
     }
 }
--- src/main/groovy/com/fpoon/s3dcam/Main.groovy
+++ src/main/groovy/com/fpoon/s3dcam/Main.groovy
@@ -11,8 +11,22 @@ import java.awt.BorderLayout
 import java.awt.Color
 import java.awt.FlowLayout
 import java.awt.Graphics
+import java.awt.event.ContainerAdapter
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.awt.event.WindowEvent
+import java.awt.event.WindowStateListener
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
+import java.security.Key
 
 class Main {
+    static Camera camera;
+
     static void main(String[] args) {
         def swing = new SwingBuilder()
 
@@ -29,15 +43,43 @@ class Main {
                      ) {
                     boxLayout(axis:BoxLayout.Y_AXIS)
                     label(text: "SIMPLE 3D CAMERA")
-                    button(text: "RESET")
+                    //button(text: "RESET")
 
                 }
-                panel(constraints: BorderLayout.CENTER, background: Color.BLACK, id: "renderPanel")
+                panel(constraints: BorderLayout.CENTER,
+                        background: Color.BLACK,
+                        id: "renderPanel",
+                        componentResized: {camera?.render()})
             }
         }
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            void keyPressed(KeyEvent e) {
+                switch (e.keyCode) {
+                    case {it in [KeyEvent.VK_UP,
+                                 KeyEvent.VK_DOWN,
+                                 KeyEvent.VK_LEFT,
+                                 KeyEvent.VK_RIGHT,
+                                 KeyEvent.VK_W,
+                                 KeyEvent.VK_S
+                                ]}:
+                        def translations = [(KeyEvent.VK_UP): Camera.TRANSLATION_UP,
+                                            (KeyEvent.VK_DOWN): Camera.TRANSLATION_DOWN,
+                                            (KeyEvent.VK_LEFT): Camera.TRANSLATION_LEFT,
+                                            (KeyEvent.VK_RIGHT): Camera.TRANSLATION_RIGHT,
+                                            (KeyEvent.VK_W): Camera.TRANSLATION_FORWARD,
+                                            (KeyEvent.VK_S): Camera.TRANSLATION_BACKWARD
+                                        ]
+                        camera.translate(translations[e.keyCode])
+                        break
+                }
+                camera?.render()
+            }
+        })
         frame.setVisible(true)
         Scene scene = new Scene()
-        Camera camera = new Camera(swing."renderPanel", scene)
+        def renderPanel = swing."renderPanel"
+        camera = new Camera(renderPanel, scene)
         camera.render()
     }
 }
